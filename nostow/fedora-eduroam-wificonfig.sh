2c1a59e1ca5437f6c5ad794a0b6c3395135edb4c
DumbMahreeo <max@segf.net>
2023-11-20 10:27:38 +0100

    Adding fedora eduroam wifi config script

---
+++ nostow/fedora-eduroam-wificonfig.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# You need this configuration to be able to connect
+# to legacy networks configs such as eduroam or other
+# enterprise/institutional networks on Fedora
+
+[ "$(whoami)" = "root" ] || \
+    (echo "Run this script as root (sudo or doas)" && exit 1)
+
+update-crypto-policies --set LEGACY
+
+OPENSSL_CONFIG='/etc/crypto-policies/back-ends/openssl.config'
+
+sed -i.OLDCOPY 's/SECLEVEL=./SECLEVEL=0/' "$OPENSSL_CONFIG" \
+    && echo -e "\nUpdated SECLEVEL in $OPENSSL_CONFIG, config file is\n$OPENSSL_CONFIG.OLDCOPY\n\nPlease reboot the system"

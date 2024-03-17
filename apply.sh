66bc1ded5fe25eac18f299d4e429812928090fdd
DumbMahreeo <max@segf.net>
2024-03-17 21:56:30 +0000

    Fixing apply script

--- apply.sh
+++ apply.sh
@@ -4,9 +4,10 @@ conf() {
     stow --target=$HOME $1
 }
 
-command -v stow > /dev/null || echo "Please install GNU Stow" && exit 1
+command -v stow > /dev/null || (echo 'Please install GNU Stow' && exit 1)
 
 conf fish
 conf scripts
 conf systemd-services
 
+echo 'Done!'

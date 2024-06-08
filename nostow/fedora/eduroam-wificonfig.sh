01a507f2da496f76e4e74984ab8984b9731a2833
DumbMahreeo <max@segf.net>
2024-06-08 15:57:12 +0200

    Updated eduroam fix script

--- nostow/fedora/eduroam-wificonfig.sh
+++ nostow/fedora/eduroam-wificonfig.sh
@@ -6,9 +6,23 @@
 
 [ "$(whoami)" != "root" ] && exec echo "Run this script as root (pkexec, doas or sudo)"
 
-update-crypto-policies --set LEGACY
+#update-crypto-policies --set LEGACY
+#
+#OPENSSL_CONFIG='/etc/crypto-policies/back-ends/openssl.config'
+#
+#sed -i.OLDCOPY 's/SECLEVEL=./SECLEVEL=0/' "$OPENSSL_CONFIG" \
+#    && echo -e "\nUpdated SECLEVEL in $OPENSSL_CONFIG, config file is\n$OPENSSL_CONFIG.OLDCOPY\n\nPlease reboot the system"
 
-OPENSSL_CONFIG='/etc/crypto-policies/back-ends/openssl.config'
+nmcli connection modify eduroam 802-1x.phase1-auth-flags 32 || exec echo "You need to try and connect at least once first, even if the connection fails"
 
-sed -i.OLDCOPY 's/SECLEVEL=./SECLEVEL=0/' "$OPENSSL_CONFIG" \
-    && echo -e "\nUpdated SECLEVEL in $OPENSSL_CONFIG, config file is\n$OPENSSL_CONFIG.OLDCOPY\n\nPlease reboot the system"
+echo "Changed eduroam phase1-auth settings"
+
+printf "NetworkManager.service should be restarted now, is that ok? [Y/n]: "
+
+read dorestart
+
+dorestart=`printf "$dorestart" | tr '[:upper:]' '[:lower:]'`
+
+[ "$dorestart" != n ] && systemctl restart NetworkManager.service && exec echo "Restarting NetworkManager.service"
+
+echo "Didn't restart NetworkManager.service"
